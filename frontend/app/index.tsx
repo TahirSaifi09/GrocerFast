42f155cff37db33fd4fec6d5eb1ed305882be55d
Tahir Saifi <tahirsaifi413@gmail.com>
2025-10-17 18:51:02 +0530

    Correct design of category slider

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
 import Header from "./components/Header";
 import MightNeed from "./components/home/might-need";
+import CategorySlider from "./components/home/catogery-slider";
 
 export default function Index() {
   return (
     <View>
       <Header />
+      {/* <CategorySlider /> */}
       <MightNeed />
     </View>
   );
